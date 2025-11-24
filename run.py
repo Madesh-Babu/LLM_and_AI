d32244f0125607c10444f2588dc8f7bc3aad1a3c
Madesh Babu <madeshbabu@bitcot.com>
2025-11-24 10:25:53 +0530

    update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from src import create_app
+from src.services.rag_chain import build_rag_chain
 
 app = create_app()
+
+with app.app_context():
+    rag_chain = build_rag_chain()
+    app.config["RAG_CHAIN"] = rag_chain
+    print("RAG INITIALIZED:", rag_chain)
 
 if __name__ == "__main__":
     app.run(debug=True)
